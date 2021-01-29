46e9ec6d17f9aaf0463f4029a670809f5ddb6517
DESKTOP-1CRUKJD\user <qowoeo948@gmail.com>
2021-01-29 17:55:06 +0900

    ver21-01-29

--- ActionBarApp/app/src/main/java/com/koreait/actionbarapp/gallery/GalleryAdapter.java
+++ ActionBarApp/app/src/main/java/com/koreait/actionbarapp/gallery/GalleryAdapter.java
@@ -1,26 +1,73 @@
 package com.koreait.actionbarapp.gallery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.koreait.actionbarapp.MainActivity;
+import com.koreait.actionbarapp.R;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class GalleryAdapter extends BaseAdapter {
-    //리소스에 있는 자원 이용..을 하지 않고 이제 uri에서 떙겨와야함
-    //네트워크 http요청? socket 요청?   --> http 로.... 웹서버 구축..
+    String TAG = this.getClass().getName();
+    MainActivity mainActivity;
+    ArrayList<Gallery> galleryList = new ArrayList<Gallery>();
+    LayoutInflater layoutInflater;
+
+    public GalleryAdapter(MainActivity mainActivity){
+        this.mainActivity = mainActivity;   //인플레이터를 얻기위해
+        layoutInflater=mainActivity.getLayoutInflater();
+    }
+
 
     public int getCount() {
-        return 0;
+        return galleryList.size();
     }
 
     public Object getItem(int position) {
-        return null;
+        return galleryList.get(position);
     }
 
     public long getItemId(int position) {
-        return 0;
+        Gallery gallery = galleryList.get(position);
+        return gallery.getGallery_id();
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View view = null;//미 메서드에서 최종 반환할 뷰
+        if(convertView==null){   //레이아웃 뷰가 존재하지 않는다면, 인플레이션 시킴
+            //false의 의미: 지정한 parent에 부착하지 않고, 인플레이션 대상 xml의 최상위를 반환
+            view = layoutInflater.inflate(R.layout.item_gallery,parent,false);
+        }else{  //이미 존재한다면, 기존 뷰 그대로 재사용
+            view = convertView;
+        }
+
+        //리스트에 들어있는 position번쨰 Galley추출
+        Gallery gallery = galleryList.get(position);
+
+        ImageView img = view.findViewById(R.id.img);
+        TextView t_title = view.findViewById(R.id.t_title);
+
+        img.setImageBitmap(gallery.getBitmap());
+        t_title.setText(gallery.getTitle());
+
+        view.setOnClickListener(e->{
+            Toast.makeText(mainActivity,gallery.getGallery_id()+"선택했어?",Toast.LENGTH_SHORT).show();
+        });
+
+        return view;
+
     }
+
 }
